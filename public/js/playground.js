78c0a33be4b5564481bcbeaaee081cebc1e01c7f
suuuth <suther98@msu.edu>
2019-05-12 13:55:24 -0400

    adding fancy console.log text to homepage

--- public/js/playground.js
+++ public/js/playground.js
@@ -68,12 +68,12 @@ window.addEventListener('scroll', function (e) {
 // * display welcome message on home page *
 // *
 
-// if ( document.querySelector('.url-js').getAttribute('url') == '/' ) {
+if ( document.querySelector('.url-js').getAttribute('url') == '/' ) {
 
-//   function hireMe () {
-//     window.location.href = window.location.href + 'resume'
-//   }
+  function hireMe () {
+    window.location.href = window.location.href + 'resume'
+  }
 
-//   console.log('%c Hello!', 'font-weight: bold; font-size: 50px; text-shadow:1px 1px 0px black, 1px -1px 0px black, -1px 1px 0px black, -1px -1px 0px black;')
-//   console.log('%c I really love having a job!  If you would like to give me one, just type %chireMe() %cin the console here to find all the information you may need!  Have a day!', 'font-weight: normal; font-size: 20px; line-height: 1.2;', 'font-weight: bold; font-size: 26px; line-height: 1.2; text-shadow:1px 1px 0px black, 1px -1px 0px black, -1px 1px 0px black, -1px -1px 0px black;', 'font-weight: normal; font-size: 20px; line-height: 1.2;')
-// }
+  console.log('%c Hello!', 'font-weight: bold; font-size: 50px; text-shadow:1px 1px 0px black, 1px -1px 0px black, -1px 1px 0px black, -1px -1px 0px black;')
+  console.log('%c I really love having a job!  If you would like to give me one, just type %chireMe() %cin the console here to find all the information you may need!  Have a day!', 'font-weight: normal; font-size: 20px; line-height: 1.2;', 'font-weight: bold; font-size: 26px; line-height: 1.2; text-shadow:1px 1px 0px black, 1px -1px 0px black, -1px 1px 0px black, -1px -1px 0px black;', 'font-weight: normal; font-size: 20px; line-height: 1.2;')
+}
