1f4005cb8f9dc4441126586a116506b649163ba4
suuuth <suther98@msu.edu>
2019-05-12 20:31:36 -0400

    improving UI

--- public/js/about-me.js
+++ public/js/about-me.js
@@ -1,10 +1,18 @@
 
-function aboutMeScroll () {
+// function aboutMeScroll () {
+//     var scrollAbout = header.clientHeight
+//     window.scroll({
+//         top: scrollAbout,
+//         behavior: 'smooth'
+//     })
+// }
+
+// document.querySelector('.about-me-js').addEventListener('click', aboutMeScroll)
+document.querySelector('.about-me-js').addEventListener('click', function () {
     var scrollAbout = header.clientHeight
     window.scroll({
         top: scrollAbout,
         behavior: 'smooth'
     })
-}
-
-document.querySelector('.about-me-js').addEventListener('click', aboutMeScroll)
+    this.blur()
+})
