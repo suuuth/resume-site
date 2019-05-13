870186b9fe9b3b054974af95d76fbcb295f2e66c
suuuth <suther98@msu.edu>
2019-05-12 20:52:03 -0400

    making nav button easier to see

--- public/js/about-me.js
+++ public/js/about-me.js
@@ -1,13 +1,4 @@
 
-// function aboutMeScroll () {
-//     var scrollAbout = header.clientHeight
-//     window.scroll({
-//         top: scrollAbout,
-//         behavior: 'smooth'
-//     })
-// }
-
-// document.querySelector('.about-me-js').addEventListener('click', aboutMeScroll)
 document.querySelector('.about-me-js').addEventListener('click', function () {
     var scrollAbout = header.clientHeight
     window.scroll({
